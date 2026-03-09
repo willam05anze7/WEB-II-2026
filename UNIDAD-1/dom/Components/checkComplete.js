9b4e5750dc0e104f32072566b87af6b0f0c4aa06
Willan <willam05anze7@gmail.com>
2026-03-09 09:42:17 -0400

    Dom intermedio

---
+++ UNIDAD-1/dom/Components/checkComplete.js
@@ -0,0 +1,17 @@
+const checkComplete = ()=>{
+        const i = document.createElement('i');
+        i.classList.add('far', 'fa-check-square', 'icon');//estilo de icono
+        i.addEventListener('click', color);
+        return i;
+    }
+
+//programacion a componenetes 
+
+    const color =(evento)=>{     // cambio de color al check
+        const element = evento.target;
+        element.classList.add('fas');
+        element.classList.add('completeIcon');
+        element.classList.remove('far');
+    }
+
+export default checkComplete;
---
+++ UNIDAD-1/dom/script.js
@@ -0,0 +1,56 @@
+import checkComplete from "./Components/checkComplete.js";
+import deleteIcon from "./Components/delete.js";
+
+(()=>{
+    const btn = document.querySelector('[data-form-btn]');
+    console.log(btn);
+
+    const createTask= (evento)=>{
+        evento.preventDefault();
+        const input = document.querySelector('[data-form-input]');
+        const value = input.value;//recupero el valor del input
+        const list = document.querySelector('[data-list]');
+        const task = document.createElement('li');
+        task.classList.add('card');
+        input.value = '';
+        const contTaks = document.createElement('div');
+        const titleTask =document.createElement('span');
+        titleTask.classList.add('task');
+        titleTask.innerText = value;
+        contTaks.appendChild(checkComplete());//agrego el check al hijo del div
+        contTaks.appendChild(titleTask);
+        task.appendChild(contTaks);
+        task.appendChild(deleteIcon());
+        list.appendChild(task);
+        
+    }
+    //llamar a crear task
+    btn.addEventListener('click', createTask);
+    /*
+    const checkComplete = ()=>{
+        const i = document.createElement('i');
+        i.classList.add('far', 'fa-check-square', 'icon');//estilo de icono
+        i.addEventListener('click', color);
+        return i;
+    }
+
+    const color =(evento)=>{     // cambio de color al check
+        const element = evento.target;
+        element.classList.add('fas');
+        element.classList.add('completeIcon');
+        element.classList.remove('far');
+    }
+
+    const deleteIcon =()=>{
+        const i = document.createElement('i');
+        i.classList.add('fas', 'fa-trash-alt', 'icon');  
+        i.addEventListener('click', deleteTask);
+        return i;
+    }
+
+    const deleteTask =(evento)=>{
+        const parent = evento.target.parentElement; //selecciona el padre del elemento
+        parent.remove();
+    }
+*/
+})();
